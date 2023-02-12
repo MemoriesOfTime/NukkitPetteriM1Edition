9f53ebeee2cc1b05adae6e06cca2bc253c658a34
LT_Name <572413378@qq.com>
2023-02-12 23:09:26 +0800

    1.19.50

--- src/main/java/cn/nukkit/inventory/CraftingManager.java
+++ src/main/java/cn/nukkit/inventory/CraftingManager.java
@@ -44,6 +44,7 @@ public class CraftingManager {
     public static DataPacket packet527;
     public static DataPacket packet544;
     public static DataPacket packet554;
+    public static DataPacket packet560;
 
     private final Map<Integer, Map<UUID, ShapedRecipe>> shapedRecipes313 = new Int2ObjectOpenHashMap<>();
     private final Map<Integer, Map<UUID, ShapedRecipe>> shapedRecipes332 = new Int2ObjectOpenHashMap<>();
@@ -470,6 +471,7 @@ public class CraftingManager {
     }
 
     public void rebuildPacket() {
+        packet560 = packetFor(560);
         packet554 = packetFor(554);
         packet544 = packetFor(544);
         packet527 = packetFor(527);
